23f1ddaa589ce9162e0dc90b8c92969c4492753b
Micha Lehmann <michalehmann0112@gmail.com>
2022-09-06 20:15:46 +0200

    Simplify translate command

--- src/main/kotlin/discord/commands/Translate.kt
+++ src/main/kotlin/discord/commands/Translate.kt
@@ -10,23 +10,15 @@ object Translate : Command() {
     override val name = "translate"
     override val description = "Translate a message to the chat!"
 
-    private val targetLang by StringParam(
-        "The language you want to translate to",
-        TargetLang.values().slice(0..24).map(TargetLang::name)
-    )
-
     private val message by StringParam("The message you want to translate.")
 
-    private val sourceLang by StringParam("Your language").Optional()
+    private val guildLang = TargetLang.AmericanEnglish
 
     override suspend fun execute() {
         val behavior = interaction.deferPublicResponse()
 
         behavior.respond {
-            if (sourceLang != null)
-                content = deepl.translate(message, TargetLang.valueOf(targetLang), SourceLang.valueOf(sourceLang!!))
-            else
-                content = deepl.translate(message, TargetLang.valueOf(targetLang))
+            content = deepl.translate(message, guildLang)
         }
     }
 
