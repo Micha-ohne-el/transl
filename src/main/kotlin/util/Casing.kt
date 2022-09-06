7252b96547ccd7f51d4b92b5c31cd39de6b08458
Micha Lehmann <michalehmann0112@gmail.com>
2022-09-06 21:01:24 +0200

    Improve casing utility and add tests

--- src/main/kotlin/util/Casing.kt
+++ src/main/kotlin/util/Casing.kt
@@ -1,5 +1,25 @@
 package util
 
-fun String.toSnakeCase(): String = Regex("[A-Z]").replace(this) { match ->
-    "_" + match.value.lowercase()
+fun String.toSnakeCase() = splitWords(this).joinToSnakeCase()
+
+fun String.toKebabCase() = splitWords(this).joinToKebabCase()
+
+fun String.toCamelCase() = splitWords(this).joinToCamelCase()
+
+fun String.toPascalCase() = splitWords(this).joinToPascalCase()
+
+
+private fun splitWords(text: String): List<String> {
+    return text.split(Regex("""\s+|_|-|(?<!^)(?=[A-Z])""")).map(String::lowercase)
 }
+
+private fun List<String>.joinToSnakeCase() = joinToString("_")
+
+private fun List<String>.joinToKebabCase() = joinToString("-")
+
+private fun List<String>.joinToCamelCase() =
+    get(0) + subList(1, size).joinToString(separator = "", transform = String::titlecase)
+
+private fun List<String>.joinToPascalCase() = joinToString(separator = "", transform = String::titlecase)
+
+private fun String.titlecase() = replaceFirstChar { char -> char.uppercase() }
---
+++ src/test/kotlin/util/CasingTest.kt
@@ -0,0 +1,98 @@
+package util
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.datatest.withData
+import io.kotest.matchers.shouldBe
+
+class CasingTest : DescribeSpec() {
+    init {
+        context("for empty string input") {
+            val input = ""
+
+            describe("toSnakeCase") {
+                it("returns empty string") {
+                    input.toSnakeCase() shouldBe ""
+                }
+            }
+
+            describe("toKebabCase") {
+                it("returns empty string") {
+                    input.toKebabCase() shouldBe ""
+                }
+            }
+
+            describe("toCamelCase") {
+                it("returns empty string") {
+                    input.toCamelCase() shouldBe ""
+                }
+            }
+
+            describe("toPascalCase") {
+                it("returns empty string") {
+                    input.toPascalCase() shouldBe ""
+                }
+            }
+        }
+
+        context("for 'test' input") {
+            val input = "test"
+
+            describe("toSnakeCase") {
+                it("returns 'test'") {
+                    input.toSnakeCase() shouldBe "test"
+                }
+            }
+
+            describe("toKebabCase") {
+                it("returns 'test'") {
+                    input.toKebabCase() shouldBe "test"
+                }
+            }
+
+            describe("toCamelCase") {
+                it("returns 'test'") {
+                    input.toCamelCase() shouldBe "test"
+                }
+            }
+
+            describe("toPascalCase") {
+                it("returns 'Test'") {
+                    input.toPascalCase() shouldBe "Test"
+                }
+            }
+        }
+
+        context("for complex input") {
+            withData(
+                "this_is_a_test",
+                "this-is-a-test",
+                "thisIsATest",
+                "ThisIsATest"
+            ) {
+                this@context.describe("toSnakeCase") {
+                    it("returns 'this_is_a_test'") {
+                        it.toSnakeCase() shouldBe "this_is_a_test"
+                    }
+                }
+
+                this@context.describe("toKebabCase") {
+                    it("returns 'this-is-a-test'") {
+                        it.toKebabCase() shouldBe "this-is-a-test"
+                    }
+                }
+
+                this@context.describe("toCamelCase") {
+                    it("returns 'thisIsATest'") {
+                        it.toCamelCase() shouldBe "thisIsATest"
+                    }
+                }
+
+                this@context.describe("toPascalCase") {
+                    it("returns 'ThisIsATest'") {
+                        it.toPascalCase() shouldBe "ThisIsATest"
+                    }
+                }
+            }
+        }
+    }
+}
