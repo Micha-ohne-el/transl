2184b3f1d2626124c1b006e65de2201fcb58671e
Micha Lehmann <michalehmann0112@gmail.com>
2022-09-08 16:17:28 +0200

    Remove optional params for now

--- src/main/kotlin/discord/params/StringParam.kt
+++ src/main/kotlin/discord/params/StringParam.kt
@@ -1,8 +1,6 @@
 package discord.params
 
-import dev.kord.rest.builder.interaction.OptionsBuilder
 import dev.kord.rest.builder.interaction.StringChoiceBuilder
-import discord.params.base.OptionalParam
 import discord.params.base.RequiredParam
 
 class StringParam(
@@ -16,19 +14,6 @@ class StringParam(
         if (allowedChoices != null) {
             for (c in allowedChoices) {
                 choice(c, c)
-            }
-        }
-    }
-
-    inner class Optional : OptionalParam<String>() {
-        override val option: OptionsBuilder
-        get() = StringChoiceBuilder(name, description).apply {
-            required = false
-
-            if (allowedChoices != null) {
-                for (c in allowedChoices) {
-                    choice(c, c)
-                }
             }
         }
     }
