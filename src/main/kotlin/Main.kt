cc586af05799cf57ae88fb91e6cbfb123d0fe8e2
Micha Lehmann <michalehmann0112@gmail.com>
2022-11-13 18:54:03 +0100

    Add some logging to see how many guilds TransL is in

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,23 @@
 import dev.kord.core.Kord
+import dev.kord.core.event.gateway.ReadyEvent
+import dev.kord.core.event.guild.GuildCreateEvent
 import dev.kord.core.on
 import discord.commands.GetGuildLang
 import discord.commands.SetGuildLang
 import discord.commands.Translate
-import discord.commands.TranslateFrom
 import discord.commands.TranslateBetween
+import discord.commands.TranslateFrom
 import discord.commands.TranslateMessage
 import discord.commands.TranslateTo
 import discord.completeSuggestions
 import discord.executeCommand
 import discord.registerCommandsForGuild
 import discord.registerCommandsGlobally
+import io.ktor.http.quote
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+val log: Logger = LoggerFactory.getLogger("Main")
 
 suspend fun main() {
     val kord = Kord(Vault.discordBotToken)
@@ -26,6 +33,14 @@ suspend fun main() {
     kord.on(kord, ::executeCommand)
 
     kord.on(kord, ::completeSuggestions)
+
+    kord.on<GuildCreateEvent> {
+        log.info("TransL was added to a guild: ${guild.name.quote()} (${guild.memberCount} members)")
+    }
+
+    kord.on<ReadyEvent> {
+        log.info("TransL is part of ${guildIds.size} guilds!")
+    }
 
     kord.login()
 }
