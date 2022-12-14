21697bd156969d15268503e74cc2061d06b3d826
Micha Lehmann <michalehmann0112@gmail.com>
2022-12-14 20:50:03 +0100

    Add error handling to TranslateMessage and TranslateForMe

--- src/main/kotlin/discord/commands/TranslateForMe.kt
+++ src/main/kotlin/discord/commands/TranslateForMe.kt
@@ -1,9 +1,15 @@
 package discord.commands
 
 import TranslationRepo
+import deepl.SourceLang.English
 import dev.kord.core.behavior.interaction.response.respond
 import discord.MessageCommand
 import discord.toTargetLang
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import util.respondWithError
+
+private val log: Logger = LoggerFactory.getLogger("TranslateForMe")
 
 object TranslateForMe : MessageCommand() {
     override val name = "Translate for me"
@@ -11,11 +17,51 @@ object TranslateForMe : MessageCommand() {
     override suspend fun execute() {
         val behavior = interaction.deferEphemeralResponse()
 
-        behavior.respond {
-            content = TranslationRepo.translate(
-                interaction.messages.values.first().content,
-                interaction.locale!!.toTargetLang()
-            )
+        val targetLang = interaction.locale!!.toTargetLang()
+
+        val message = interaction.messages.values.firstOrNull()
+
+        try {
+            if (message?.content.isNullOrEmpty()) {
+                log.warn("Empty message could not be translated. $message")
+
+                behavior.respondWithError {
+                    field {
+                        name = TranslationRepo.translate("Sorry", targetLang, English)
+                        value = TranslationRepo.translate(
+                            """
+                                This message cannot be translated as it does not contain regular text.
+                                Text in Embeds cannot yet be translated.
+                                """,
+                            targetLang,
+                            English
+                        )
+                    }
+                }
+
+                return
+            }
+
+            behavior.respond {
+                content = TranslationRepo.translate(message!!.content, targetLang)
+            }
+        } catch (error: Throwable) {
+            log.error("Message could not be translated. $message")
+
+            behavior.respondWithError {
+                field {
+                    name = TranslationRepo.translate("Sorry", targetLang, English)
+                    value = TranslationRepo.translate(
+                        """
+                            An error occurred while translating that message, please try again later.
+                        """,
+                        targetLang,
+                        English
+                    )
+                }
+            }
+
+            throw error
         }
     }
 }
--- src/main/kotlin/discord/commands/TranslateMessage.kt
+++ src/main/kotlin/discord/commands/TranslateMessage.kt
@@ -2,10 +2,16 @@ package discord.commands
 
 import GuildLangRepo
 import TranslationRepo
+import deepl.SourceLang.English
 import dev.kord.core.behavior.interaction.response.respond
 import discord.MessageCommand
 import discord.toTargetLang
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import util.respondWithError
 import util.toLongOrNull
+
+private val log: Logger = LoggerFactory.getLogger("TranslateMessage")
 
 object TranslateMessage : MessageCommand() {
     override val name = "Translate message"
@@ -19,8 +25,49 @@ object TranslateMessage : MessageCommand() {
             ?: interaction.guildLocale?.toTargetLang()
             ?: GuildLangRepo.defaultGuildLang
 
-        behavior.respond {
-            content = TranslationRepo.translate(interaction.messages.values.first().content, guildLang)
+        val message = interaction.messages.values.firstOrNull()
+
+        try {
+            if (message?.content.isNullOrEmpty()) {
+                log.warn("Empty message could not be translated. $message")
+
+                behavior.respondWithError {
+                    field {
+                        name = TranslationRepo.translate("Sorry", guildLang, English)
+                        value = TranslationRepo.translate(
+                            """
+                                This message cannot be translated as it does not contain regular text.
+                                Text in Embeds cannot yet be translated.
+                                """,
+                            guildLang,
+                            English
+                        )
+                    }
+                }
+
+                return
+            }
+
+            behavior.respond {
+                content = TranslationRepo.translate(message!!.content, guildLang)
+            }
+        } catch (error: Throwable) {
+            log.error("Message could not be translated. $message")
+
+            behavior.respondWithError {
+                field {
+                    name = TranslationRepo.translate("Sorry", guildLang, English)
+                    value = TranslationRepo.translate(
+                        """
+                            An error occurred while translating that message, please try again later.
+                        """,
+                        guildLang,
+                        English
+                    )
+                }
+            }
+
+            throw error
         }
     }
 }
