4ccc44ce724d4933b2113034cf55c7bb741886ad
Micha Lehmann <michalehmann0112@gmail.com>
2023-03-26 16:03:24 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,20 +14,20 @@ repositories {
 
 dependencies {
     implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
-    implementation("dev.kord:kord-core:0.8.0-M17")
+    implementation("dev.kord:kord-core:0.8.1")
     implementation("io.ktor:ktor-client-logging:2.2.2")
     implementation("io.ktor:ktor-client-content-negotiation:2.2.2")
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.2.2")
     implementation("org.slf4j:slf4j-simple:2.0.6")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
     implementation("org.jetbrains.exposed:exposed-core:0.41.1")
     implementation("org.jetbrains.exposed:exposed-jdbc:0.41.1")
     implementation("org.jetbrains.exposed:exposed-dao:0.41.1")
-    implementation("org.postgresql:postgresql:42.5.1")
+    implementation("org.postgresql:postgresql:42.5.4")
 
-    testImplementation("io.kotest:kotest-runner-junit5:5.5.4")
-    testImplementation("io.kotest:kotest-assertions-core:5.5.4")
-    testImplementation("io.kotest:kotest-framework-datatest:5.5.4")
+    testImplementation("io.kotest:kotest-runner-junit5:5.5.5")
+    testImplementation("io.kotest:kotest-assertions-core:5.5.5")
+    testImplementation("io.kotest:kotest-framework-datatest:5.5.5")
 }
 
 tasks.test {
