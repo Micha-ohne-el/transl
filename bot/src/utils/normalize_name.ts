01691f8b9bab2a2b51f0efeae32f97ddbf7f9517
Mika <mika@handhold.ing>
2025-11-25 22:08:15 +0100

    Lowercase names when normalizing

--- bot/src/utils/normalize_name.ts
+++ bot/src/utils/normalize_name.ts
@@ -1,3 +1,3 @@
 export function normalizeName(name: string): string {
-	return name.replaceAll(/[^\p{Ll}\p{Lm}\p{Lo}\p{N}\p{sc=Devanagari}\p{sc=Thai}_-]+/gu, "_");
+	return name.toLowerCase().replaceAll(/[^\p{Ll}\p{Lm}\p{Lo}\p{N}\p{sc=Devanagari}\p{sc=Thai}_-]+/gu, "_");
 }
